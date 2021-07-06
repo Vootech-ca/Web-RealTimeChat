f91baf0b71e7ade33e2c56cff9463c8cafc773d6
Bashdar Umar <62844215+DevBazho@users.noreply.github.com>
2021-07-06 17:23:16 +0300

    Delete .history directory

--- .history/index_20210509042220.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("SERVER up and Running on PORT "+ port));
--- .history/index_20210509042524.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-
-// MIDDLEWARES
-app.use(express.json());
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("SERVER up and Running on PORT "+ port));
